b6c243e01fe7a72a9e13a8e10e796103051adee0
alaa <alaa@example.com>
2025-04-29 00:10:33 -0700

    تصحيح إعدادات storageBucket في firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
   projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  storageBucket: "jawado-f3c44.appspot.com",
   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "YOUR_APP_ID"
 };
